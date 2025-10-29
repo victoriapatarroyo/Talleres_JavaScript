51b691a8ed202bc224f26d2df2642fd52937b639
victoriapatarroyo <victoria.patarroyo@gmail.com>
2025-10-29 14:25:03 -0500

    Mensajes a usuario de seccines del taller y ejercicios

--- tallerJS.js
+++ tallerJS.js
@@ -1,10 +1,13 @@
 // Variables y tipo de datos
+console.log("Variables y tipos de datos");
 // Ejercicio 1
+console.log("Ejercicio 1");
 let nombre = "Victoria";
 let edad = 48;
 let esEstudiante = true;
 
 // Ejercicio 2
+console.log("Ejercicio 2");
 let sinValor;
 let valorNulo = null;
 
@@ -13,13 +16,17 @@ console.log(valorNulo);
 
 
 // Ingreso de datos por teclado
+console.log("Ingreso de datos por teclado");
+console.log("Ejercicio 1");
 // Ejercicio 1
+
 const prompt = require("prompt-sync")();
 let anioNacimiento = prompt("Escribe tu año de nacimiento: ");
 let edadUsuario = 2025 - anioNacimiento;
 console.log("Tu edad es " + edadUsuario);
 
 // Ejercicio 2
+console.log("Ejercicio 2");
 let numero1 = parseInt(prompt("Ingresa un primer número: "));
 let numero2 = parseInt(prompt("Ingresa un segundo número: "));
 let suma = numero1 + numero2;
@@ -27,6 +34,8 @@ console.log("La suma es: " + suma);
 
 
 // Condicional if
+console.log("Condicional if");
+console.log("Ejercicio 1");
 // Ejercicio 1
 let edadMayorEdad = parseInt(prompt("Ingresa tu edad: "));
 if(edadMayorEdad >= 18) {
@@ -35,9 +44,22 @@ if(edadMayorEdad >= 18) {
     console.log("No puedes entrar");
 }
 
+// Ejercicio 2
+console.log("Ejercicio 2");
+let estadoNumero = parseInt(prompt("Ingresa un número: "));
+if(estadoNumero > 0) {
+    console.log("Es positivo");
+} else if(estadoNumero <0) {
+    console.log("Es negativo");
+} else {
+    console.log("Es cero");
+}
+    
 
 // Bucle while
+console.log("Bucle while");
 // Ejercicio 1
+console.log("Ejercicio 1");
 let numeroWhile = parseInt(prompt("Ingresa un número: "))
 let i = 1;
 
@@ -47,6 +69,7 @@ while(i <= numeroWhile) {
 }
 
 // Ejercicio 2
+console.log("Ejercicio 2");
 let contrasenia = prompt("Ingresa la contraseña: ");
 let contraseniaReal = "1234";
 
@@ -62,12 +85,15 @@ while(contrasenia != contraseniaReal) {
 
 
 // Bucle for
+console.log("Bucle for");
 // Ejercicio 1
+console.log("Ejercicio 1");
 for(let i = 1; i <= 10; i++) {
     console.log(i);
 }
 
 // Ejercicio 2
+console.log("Ejercicio 2");
 let numeroMultiplicar = parseInt(prompt("Ingresa un número: "));
 
 for (i = 1; i <= 10; i++) {
